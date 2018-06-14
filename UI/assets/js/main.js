6ec8cbe5afdc5bbf0b2d3deac26f75295c7e6063
Farayola Timileyin Emmanuel <rafmme@gmail.com>
2018-06-14 14:02:20 +0100

    [#158306152] Add SignUp modal form for signing up of account on the app

--- UI/assets/js/main.js
+++ UI/assets/js/main.js
@@ -1,8 +1,12 @@
 
 const modal = document.getElementById('myModal');
 const fab = document.getElementById('fab');
-const closeBtn = document.getElementsByClassName('close')[0];
+const closeBtn = document.getElementsByClassName('close')[0]
 const addRideModal = document.getElementById('add-ride-modal');
+const loginModal = document.getElementById('login-modal');
+const loginBtn = document.getElementById('loginBtn');
+const signUpModal = document.getElementById('signup-modal');
+const signUpBtn = document.getElementById('signUpBtn');
 
 fab.onclick = () => {
 	addRideModal.style.display = "block"
@@ -10,10 +14,24 @@ fab.onclick = () => {
 	modal.style.display = "block";
 }
 
+loginBtn.onclick = () => {
+	loginModal.style.display = "block"
+	modal.appendChild(loginModal)
+	modal.style.display = "block";
+}
+
+signUpBtn.onclick = () => {
+	signUpModal.style.display = "block"
+	modal.appendChild(signUpModal)
+	modal.style.display = "block";
+}
+
 closeBtn.onclick = () => {
 	modal.innerHTML = ""
 	modal.style.display = "none";
 	addRideModal.style.display = "none"
+	loginModal.style.display = "none"
+	signUpModal.style.display = "none"
 }
 
 window.onclick = (evt) => {
@@ -21,6 +39,8 @@ window.onclick = (evt) => {
 		 modal.innerHTML = ""
 		 modal.style.display = "none";
 		 addRideModal.style.display = "none"
+		 loginModal.style.display = "none"
+		 signUpModal.style.display = "none"
 	}
 }
 
